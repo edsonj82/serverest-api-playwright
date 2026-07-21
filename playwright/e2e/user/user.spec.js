50cbfaa02442ab1559ab2a3d8fa90030cff781ce
Edson <edsonj82@outlook.com>
2026-07-20 22:29:38 -0300

    test: implement get request to show user details by ID & show list of registered users

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -4,7 +4,8 @@ import { faker } from '@faker-js/faker';
 
 //User API tests
 test.describe('POST /usuarios', () => {
-    let userId;// 1. Declaramos a variável vazia no escopo do describe
+
+    let userId, nome, email, password, administrador;// 1. Declaramos a variável vazia no escopo do describe
 
     test('it should create a new user', async ({ request }) => {
 
@@ -34,17 +35,47 @@ test.describe('POST /usuarios', () => {
         expect(responseBody).not.toHaveProperty('password');
         expect(responseBody).not.toHaveProperty('administrador');
 
-        userId = responseBody._id;// 2. Guardamos o valor do _id retornado na nossa variável de escopo
-        // 3. No próximo teste, a variável "userId" estará preenchida e pronta para uso!
-        // Garantimos que o teste anterior rodou e o ID foi salvo antes de continuar
-        //expect(userId).toBeDefined();
+    });
 
-        // Fazemos um GET passando o ID dinamicamente na URL
-        //const response = await request.get(`https://serverest.dev/usuarios/${userId}`);
+    test('it should show user details by ID', async ({ request }) => {
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        const fullName = `${firstName} ${lastName}`;
+
+        const user = {
+            nome: fullName,
+            email: faker.internet.email({ firstName, lastName }).toLowerCase(),
+            password: 'admin1234',
+            administrador: 'true'
+        };
+
+        const response = await request.post('https://serverest.dev/usuarios', {
+            data: user
+        });
+
+        expect(response.status()).toBe(201);
+
+        const responseBody = await response.json();
+        userId = responseBody._id;// 2. Guardamos o valor do _id retornado na nossa variável de escopo
+        nome = user.nome;
+        email = user.email;
+        password = user.password;
+        administrador = user.administrador;
+
+        const userResponse = await request.get(`https://serverest.dev/usuarios/${userId}`);
+
+        expect(userResponse.status()).toBe(200);
+
+        const body = await userResponse.json();
+        expect(body).toHaveProperty('_id', userId);
+        expect(body).toHaveProperty('nome', nome);
+        expect(body).toHaveProperty('email', email);
+        expect(body).toHaveProperty('password', password);
+        expect(body).toHaveProperty('administrador', administrador);
     });
 
     test('it should not create a duplicate user', async ({ request }) => {
-        
+
         const firstName = faker.person.firstName();
         const lastName = faker.person.lastName();
         const fullName = `${firstName} ${lastName}`;
@@ -83,34 +114,30 @@ test.describe('POST /usuarios', () => {
         });
 
         expect(duplicateResponse.status()).toBe(400);
-        
+
         const duplicateResponseBody = await duplicateResponse.json();
-        expect(duplicateResponseBody).toHaveProperty('message', 'Este email já está sendo usado');        
+        expect(duplicateResponseBody).toHaveProperty('message', 'Este email já está sendo usado');
     });
 
     test('it should show list of registered users', async ({ request }) => {
 
         const response = await request.get('https://serverest.dev/usuarios');
 
-        // const user = [
-        //   nome: 'Fulano da Silva',
-        //   email: 'beltrano@qa.com.br',
-        //   password: 'admin1234',
-        //   administrador: 'true',
-        //   _id: '1AmGIy7FkEaltWdO'
-        // ]
-
         expect(response.status()).toBe(200);
-        const body = await response.json();
-        expect(body.quantidade).toBeGreaterThan(0);
-        // expect(body.usuarios).toContainEqual(user);
-        expect(body.usuarios[0]).toHaveProperty('nome');
-        expect(body.usuarios[0]).toHaveProperty('email');
-        expect(body.usuarios[0]).toHaveProperty('password');
-        expect(body.usuarios[0]).toHaveProperty('administrador');
-        expect(body.usuarios[0]).toHaveProperty('_id');
 
+        const body = await response.json();
+        expect(body.quantidade).toBeGreaterThan(0); // Garantimos que a lista não está vazia (maior que 0)
+
+        body.usuarios.forEach(user => {//Iteramos por todos os usuários para validar a estrutura dos dados
+            expect(user).toHaveProperty('nome');
+            expect(user).toHaveProperty('email');
+            expect(user).toHaveProperty('password');
+            expect(user).toHaveProperty('administrador');
+            expect(user).toHaveProperty('_id');
+        });
+
+        // Validamos se o total reportado no campo 'quantidade' 
+        // é EXATAMENTE igual ao número real de itens dentro da lista
+        expect(body.quantidade).toBe(body.usuarios.length);
     });
 });
-
-
