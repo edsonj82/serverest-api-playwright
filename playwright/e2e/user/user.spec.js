2fe2ce3e9af22ce0e2ba2d7ee7a5d8675ca448aa
Edson <edsonj82@outlook.com>
2026-07-23 19:30:50 -0300

    test: implement get request to return 404 for invalid endpoint

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -367,6 +367,13 @@ test.describe('GET /usuarios', () => {
         expect(body.quantidade).toBe(body.usuarios.length);
     });
 
+    test('it should return 404 for invalid endpoint', async ({ request }) => {
+        const response = await request.get('https://serverest.dev/usuarios-invalid-endpoint');
+        expect(response.status()).toBe(404);
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', 'Endpoint não encontrado');
+    });
+
 });
 
 test.describe('GET /usuarios/{id}', () => {
