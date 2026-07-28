12e757209278bb4829db7126e987ce608ddd54d9
Edson <edsonj82@outlook.com>
2026-07-28 19:19:24 -0300

    test: implement update request to return 405 for invalid endpoint with query parameters

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -765,4 +765,19 @@ test.describe('PUT /usuarios/{id}', () => {
         const responseBody = await response.json();
         expect(responseBody).toHaveProperty('message', `Não é possível realizar PUT em /${idInvalidEndpoint}/1234567890123456. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.`);
     });
+
+    test('it should return 405 for invalid endpoint with query parameters', async ({ request }) => {
+        const idInvalidEndpoint = 'usuarios-invalid-endpoint';
+        const response = await request.put(`https://serverest.dev/${idInvalidEndpoint}?param=value`, {
+            data: {
+                nome: 'Test User',
+                email: 'test@example.com',
+                password: 'admin1234',
+                administrador: 'true'
+            }
+        });
+        expect(response.status()).toBe(405);
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', `Não é possível realizar PUT em /${idInvalidEndpoint}?param=value. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.`);
+    });
 });
