2b0c01d9ea9217113933cbd75f93d320140bdb69
Edson <edsonj82@outlook.com>
2026-07-28 19:14:52 -0300

    test: implement update request to return 405 for invalid endpoint

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -735,4 +735,19 @@ test.describe('PUT /usuarios/{id}', () => {
         const duplicateResponseBody = await duplicateResponse.json();
         expect(duplicateResponseBody).toHaveProperty('message', 'Este email já está sendo usado');
     });
+
+    test('it should return 405 for invalid endpoint', async ({ request }) => {
+        const idInvalidEndpoint = 'usuarios-invalid-endpoint';
+        const response = await request.put(`https://serverest.dev/${idInvalidEndpoint}`, {
+            data: {
+                nome: 'Test User',
+                email: 'test@example.com',
+                password: 'admin1234',
+                administrador: 'true'
+            }
+        });
+        expect(response.status()).toBe(405);
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', `Não é possível realizar PUT em /${idInvalidEndpoint}. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.`);
+    });
 });
