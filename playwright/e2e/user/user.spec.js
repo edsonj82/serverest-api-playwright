b594243a15ad3d9594300fd9591562c1fdf46ebe
Edson <edsonj82@outlook.com>
2026-07-28 19:54:37 -0300

    test(usuarios): add 200  assertion when ID is an object

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -920,4 +920,12 @@ test.describe('DELETE /usuarios/{id}', () => {
         expect(responseBody).toHaveProperty('message', 'Nenhum registro excluído');
     });
 
+    test('it should return 200 when ID is an object', async ({ request }) => {
+        const objectId = { id: '1234567890123456' };
+        const response = await request.delete(`https://serverest.dev/usuarios/${objectId}`);
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', 'Nenhum registro excluído');
+    });
+
 });
