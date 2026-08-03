162b873a4c69e596f9ff3429bdbb9203a4393639
Edson <edsonj82@outlook.com>
2026-08-03 20:15:36 -0300

    test(products): validate error message for empty quantidade field

--- playwright/e2e/user/product.spec.js
+++ playwright/e2e/user/product.spec.js
@@ -340,6 +340,27 @@ test.describe('POST /produtos', () => {
         expect(responseData.quantidade).toBe('quantidade é obrigatório');
     });
 
+    test('it should return an error when creating a product with empty required quantidade field', async ({ request }) => {
+        const incompleteProduct = {
+            nome: faker.commerce.productName(),
+            preco: faker.number.int({ min: 10, max: 1000 }),
+            descricao: faker.commerce.productDescription(),
+            quantidade: '' // quantidade is empty
+        };
+        const response = await request.post('https://serverest.dev/produtos', {
+            data: incompleteProduct,
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': authorization
+            }
+        });
+        expect(response.status()).toBe(400);
+
+        const responseData = await response.json();
+        expect(responseData).toHaveProperty('quantidade'); // Verifica se a resposta contém a propriedade 'quantidade'
+        expect(responseData.quantidade).toBe('quantidade deve ser um número');
+    });
+
     test('it should return an error when creating a product with missing required administrador field', async ({ request }) => {
         const incompleteProduct = {
             nome: faker.commerce.productName(),
