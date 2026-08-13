06b515bccbbe2726480a5765aafbdd070066236b
Edson <edsonj82@outlook.com>
2026-08-13 20:18:21 -0300

    test(users): update GET invalid endpoint with trailing slash assertion from 404 to 405

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -429,12 +429,12 @@ test.describe('GET /usuarios', () => {
         expect(responseBody).toHaveProperty('message', 'Não é possível realizar GET em /usuarios-invalid-endpoint/. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.');
     });
 
-    test('it should return 404 for invalid endpoint with ID and trailing slash', async ({ request }) => {
+    test('it should return 405 for invalid endpoint with ID and trailing slash', async ({ request }) => {
         const invalidId = '1234567890123456';
         const response = await request.get(`https://serverest.dev/usuarios-invalid-endpoint/${invalidId}/`);
-        expect(response.status()).toBe(404);
+        expect(response.status()).toBe(405);
         const responseBody = await response.json();
-        expect(responseBody).toHaveProperty('message', 'Endpoint não encontrado');
+        expect(responseBody).toHaveProperty('message', 'Não é possível realizar GET em /usuarios-invalid-endpoint/. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.');
     });
 
     test('it should return 404 for invalid endpoint with query parameters and trailing slash', async ({ request }) => {
