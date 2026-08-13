b4a8e53eda571942d02b58818f28a3cc62622f63
Edson <edsonj82@outlook.com>
2026-08-13 20:12:33 -0300

    test(users): update GET invalid endpoint with ID assertion from 404 to 405

--- playwright/e2e/user/user.spec.js
+++ playwright/e2e/user/user.spec.js
@@ -399,12 +399,12 @@ test.describe('GET /usuarios', () => {
         expect(responseBody).toHaveProperty('message', 'Não é possível realizar GET em /usuarios-invalid-endpoint. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.');
     });
 
-    test('it should return 404 for invalid endpoint with ID', async ({ request }) => {
+    test('it should return 405 for invalid endpoint with ID', async ({ request }) => {
         const invalidId = '1234567890123456';
         const response = await request.get(`https://serverest.dev/usuarios-invalid-endpoint/${invalidId}`);
-        expect(response.status()).toBe(404);
+        expect(response.status()).toBe(405);
         const responseBody = await response.json();
-        expect(responseBody).toHaveProperty('message', 'Endpoint não encontrado');
+        expect(responseBody).toHaveProperty('message', 'Não é possível realizar GET em /usuarios-invalid-endpoint/1234567890123456. Acesse https://serverest.dev para ver as rotas disponíveis e como utilizá-las.');
     });
 
     test('it should return 404 for invalid endpoint with query parameters', async ({ request }) => {
