b365ad1af7b047a5294fda0a5de325ae6676295f
Edson <edsonj82@outlook.com>
2026-09-23 06:31:30 -0300

    refactor(factories): update user factory to getUser(GET /carrinhos/:id) for user payload generation

--- playwright/e2e/user/shopping_cart.spec.js
+++ playwright/e2e/user/shopping_cart.spec.js
@@ -836,7 +836,7 @@ test.describe('GET /carrinhos/:id', () => {
 
     test.beforeAll(async ({ request }) => {
         // 1. Dados do usuário Administrador
-        const user = getUserAdmin();
+        const user = getUser();
 
         // 2. Criar usuário admin
         const response = await request.post('https://serverest.dev/usuarios', {
